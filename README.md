97269029b9b36b198db7b9b09df844ffc538f557
Chris Crowther <hikari@hikari.org.uk>
2024-04-06 16:51:28 +0100

    Initial commit.

---
+++ README.md
@@ -0,0 +1,6 @@
+SolisCloudProxy
+===============
+
+A Python module to provide access to the [Solis Cloud](https://www.soliscloud.com) Monitoring REST API, for collecting data from an installation (currently just inverter data).
+
+Heavilly inspired by [pyPowerWall](https://github.com/jasonacox/pypowerwall).
---
+++ src/SolisCloudProxy/Program.cs
@@ -0,0 +1,54 @@
+// <summary>
+//  Defines the main program entry point.
+// </summary>
+// <copyright>
+//  Copyright &copy; 2024 Chris Crowther
+// </copyright>
+//
+// MIT License
+// ===========
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy of
+// this software and associated documentation files (the �Software�), to deal in
+// the Software without restriction, including without limitation the rights to use,
+// copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the 
+// Software, and to permit persons to whom the Software is furnished to do so,
+// subject to the following conditions:
+//
+// The above copyright notice and this permission notice (including the next paragraph)
+// shall be included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+// OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+// OTHER DEALINGS IN THE SOFTWARE.
+// 
+// LicenseRef-MIT
+
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+WebApplication app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
